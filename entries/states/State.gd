26694a7a4a8d82dcf8a717ee48bd6f36bab87a1d
Scatten2022 <98631863+Scatten2022@users.noreply.github.com>
2024-01-12 03:05:41 +0800

    [Initialize] Initialize project information. (#1)
    * [Init] Init project
    
    * [Init] Add assets
    
    * [Init] Add state machine and base state class

---
+++ entries/states/State.gd
@@ -0,0 +1,19 @@
+class_name State
+extends Node
+
+signal Transitioned
+
+func Enter() -> void:
+	pass
+
+func Exit() -> void:
+	pass
+
+func Update(delta: float) -> void:
+	pass
+
+func Physics_update(delta: float) -> void:
+	pass
+
+func Process_last_frame_of_loop_animation() -> void:
+	pass
