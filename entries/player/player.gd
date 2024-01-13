a607888c14aa7d2a4ba27abe0e32e2bca5dcaf5e
swingcat <swimmingcat2021@163.com>
2024-01-13 16:10:12 +0800

    [BugFix] Fix the bug where the player's image fails to play correctly when switching states.

--- entries/player/player.gd
+++ entries/player/player.gd
@@ -9,16 +9,3 @@ const SPEED: float = 300
 
 var previous_direction: Vector2 = Vector2.DOWN
 
-
-func _physics_process(delta: float) -> void:
-	if velocity.x != 0 and velocity.y != 0:
-		animation_player.play("move_side")
-		graphics.scale.x = 1 if velocity.x > 0 else -1
-	elif velocity.y > 0:
-		animation_player.play("move_down")
-	elif velocity.y < 0:
-		animation_player.play("move_up")
-	else:
-		animation_player.play("idle_down")
-	
-	move_and_slide()
